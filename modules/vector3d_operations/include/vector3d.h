7f64133c87e8555eef522af3b4c3a64bff702241
Marina-Labinskaya <life.joy.spring@gmail.com>
2022-04-22 14:03:31 +0300

    fix style

--- modules/vector3d_operations/include/vector3d.h
+++ modules/vector3d_operations/include/vector3d.h
@@ -9,15 +9,14 @@ class Vector3d {
 protected:
 	std::vector<double> data;
 public:
-	Vector3d();
-	Vector3d(double x, double y, double z);
-	Vector3d(const Vector3d& v);
-	double& operator[](double idx);
-	const double& operator[](double idx) const;
-	double getNorm();
-	Vector3d getNormalization();
+    Vector3d();
+    Vector3d(double x, double y, double z);
+    Vector3d(const Vector3d& v);
+    double& operator[](double idx);
+    const double& operator[](double idx) const;
+    double getNorm();
+    Vector3d getNormalization();
 };
 
 bool operator ==(const Vector3d& v1, const Vector3d& v2);
-
 #endif  // MODULES_VECTOR3D_INCLUDE_VECTOR3D_H_
