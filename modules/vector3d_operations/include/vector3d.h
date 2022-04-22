3461a557aaf4bc802fa0aeb0dfe402375dc25627
Marina-Labinskaya <life.joy.spring@gmail.com>
2022-04-22 14:09:40 +0300

    fix style

--- modules/vector3d_operations/include/vector3d.h
+++ modules/vector3d_operations/include/vector3d.h
@@ -1,14 +1,14 @@
 // Copyright 2022 Labinskaya Marina
-#ifndef MODULES_VECTOR3D_INCLUDE_VECTOR3D_H_
-#define MODULES_VECTOR3D_INCLUDE_VECTOR3D_H_
+#ifndef MODULES_VECTOR3D_OPERATIONS_INCLUDE_VECTOR3D_H_
+#define MODULES_VECTOR3D_OPERATIONS_INCLUDE_VECTOR3D_H_
 
 #include <vector>
 #include <random>
 
 class Vector3d {
-protected:
-	std::vector<double> data;
-public:
+protected: 
+    std::vector<double> data;
+public: 
     Vector3d();
     Vector3d(double x, double y, double z);
     Vector3d(const Vector3d& v);
@@ -19,4 +19,4 @@ public:
 };
 
 bool operator ==(const Vector3d& v1, const Vector3d& v2);
-#endif  // MODULES_VECTOR3D_INCLUDE_VECTOR3D_H_
+#endif  //MODULES_VECTOR3D_OPERATIONS_INCLUDE_VECTOR3D_H_
