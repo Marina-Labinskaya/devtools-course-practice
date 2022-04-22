4cbf37b7a3bb660e2e6248e3c9e0e9c41cbb0cfa
Marina-Labinskaya <life.joy.spring@gmail.com>
2022-04-22 13:37:51 +0300

    vector3d, implementation of norm and normalization

---
+++ modules/vector3d_operations/include/vector3d.h
@@ -0,0 +1,23 @@
+// Copyright 2022 Labinskaya Marina
+#ifndef MODULES_VECTOR3D_INCLUDE_VECTOR3D_H_
+#define MODULES_VECTOR3D_INCLUDE_VECTOR3D_H_
+
+#include <vector>
+#include <random>
+
+class Vector3d {
+protected:
+	std::vector<double> data;
+public:
+	Vector3d();
+	Vector3d(double x, double y, double z);
+	Vector3d(const Vector3d& v);
+	double& operator[](double idx);
+	const double& operator[](double idx) const;
+	double getNorm();
+	Vector3d getNormalization();
+};
+
+bool operator ==(const Vector3d& v1, const Vector3d& v2);
+
+#endif  // MODULES_VECTOR3D_INCLUDE_VECTOR3D_H_
---
+++ modules/vector3d_operations/test/test_vector3d.cpp
@@ -0,0 +1,115 @@
+//Copyright 2022 Labinskaya Marina
+#include "vector3d.h"
+#include <gtest.h>
+
+TEST(Vector3d, can_create_vector3d_without_parameters)
+{
+	ASSERT_NO_THROW(Vector3d v);
+}
+
+TEST(Vector3d, can_create_vector3d_with_parameters)
+{
+	ASSERT_NO_THROW(Vector3d(1.0, 2.0, 3.0));
+}
+
+TEST(Vector3d, can_create_copy_vector3d)
+{
+	//Arrange & Act
+	Vector3d v;
+	ASSERT_NO_THROW(Vector3d v2 = v);
+}
+
+TEST(Vector3d, copy_vector3d_is_equal_his_parent)
+{
+	//Arrange & Act
+	Vector3d v;
+	Vector3d v2 = v;
+
+	//Assert
+	EXPECT_EQ(v, v2);
+}
+
+TEST(Vector3d, can_get_coords)
+{
+	//Arrange & Act
+	Vector3d v(1.0, 2.0, 3.0);
+
+	//Assert
+	EXPECT_DOUBLE_EQ(v[1], 2.0);
+}
+
+TEST(Vector3d, can_set_coords)
+{
+	//Arrange
+	Vector3d v;
+
+	//Act
+	v[2] = 1.5;
+
+	//Assert
+	EXPECT_DOUBLE_EQ(v[2], 1.5);
+}
+
+TEST(Vector3d, cant_get_coords_out_of_range)
+{
+	//Arrange & Act
+	Vector3d v(1.0, 2.0, 3.0);
+
+	//Assert
+	ASSERT_ANY_THROW(v[3]);
+}
+
+TEST(Vector3d, cant_set_coords_out_of_range)
+{
+	//Arrange & Act
+	Vector3d v(1.0, 2.0, 3.0);
+
+	//Assert
+	ASSERT_ANY_THROW(v[3] = 1.0);
+}
+
+TEST(Vector3d, can_get_norm_of_vector3d)
+{
+	//Arrange
+	Vector3d v(2.0, 6.0, 3.0);
+
+	//Act
+	double norm = v.getNorm();
+
+	//Assert
+	EXPECT_NEAR(norm, 7, 1e-9);
+}
+
+TEST(Vector3d, can_detect_equal_vectors3d)
+{
+	//Arrange
+	Vector3d v(2.0, 6.0, 3.0);
+	Vector3d v2 = v;
+
+	//Assert
+	EXPECT_TRUE(v == v2);
+}
+
+TEST(Vector3d, can_detect_not_equal_vectors3d)
+{
+	//Arrange
+	Vector3d v(2.0, 6.0, 3.0);
+	Vector3d v2(2.0, 6.1, 3.0);
+
+	//Assert
+	EXPECT_FALSE(v == v2);
+}
+
+TEST(Vector3d, can_get_normalization_of_not_zero_vector3d)
+{
+	//Arrange
+	Vector3d v(2.0, 6.0, 3.0);
+	Vector3d expect_res(2.0 / 7.0, 6.0 / 7.0, 3.0 / 7.0);
+	
+
+	//Act
+	Vector3d normalization = v.getNormalization();
+
+	//Assert
+	EXPECT_EQ(normalization, expect_res);
+}
